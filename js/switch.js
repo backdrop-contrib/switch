a1c811e12c3f38f2cc12d13d169bc269a70f55c0
laryn <laryn@kragtbakker.com>
2022-11-11 17:49:47 -0600

    Result of Coder Upgrade.

--- js/switch.js
+++ js/switch.js
@@ -2,7 +2,7 @@
  * @file switch javascript file
  */
 (function ($) {
-  Drupal.behaviors.Switch = {
+  Backdrop.behaviors.Switch = {
     attach: function (context, settings) {
 
       $('.form-type-switch', context).each(function(){
@@ -18,15 +18,15 @@
       		}
 
       		// make the container
-      		container = $('<div class="iphone_switch_container" style="height:'+Drupal.settings.switch.switch_height+'px; width:'+Drupal.settings.switch.switch_width+'px; position: relative; overflow: hidden"></div>');
-          img = $('<img class="iphone_switch" style="height:'+Drupal.settings.switch.switch_height+'px; width:'+Drupal.settings.switch.switch_width+'px; background-image:url('+Drupal.settings.switch.switch_path+'); background-repeat:none; background-position:'+(state == 'on' ? 0 : -53)+'px" src="'+(state == 'on' ? Drupal.settings.switch.switch_on_container_path : Drupal.settings.switch.switch_off_container_path)+'" /></div>').appendTo(container);
+      		container = $('<div class="iphone_switch_container" style="height:'+Backdrop.settings.switch.switch_height+'px; width:'+Backdrop.settings.switch.switch_width+'px; position: relative; overflow: hidden"></div>');
+          img = $('<img class="iphone_switch" style="height:'+Backdrop.settings.switch.switch_height+'px; width:'+Backdrop.settings.switch.switch_width+'px; background-image:url('+Backdrop.settings.switch.switch_path+'); background-repeat:none; background-position:'+(state == 'on' ? 0 : -53)+'px" src="'+(state == 'on' ? Backdrop.settings.switch.switch_on_container_path : Backdrop.settings.switch.switch_off_container_path)+'" /></div>').appendTo(container);
 
           img.mouseover(function(){
-      			img.css("cursor", Drupal.settings.switch.mouse_over);
+      			img.css("cursor", Backdrop.settings.switch.mouse_over);
       		});
 
       		img.mouseout(function(){
-      			img.css("background", Drupal.settings.switch.mouse_out);
+      			img.css("background", Backdrop.settings.switch.mouse_out);
       		});
       		
       		// click handling
@@ -36,7 +36,7 @@
             img.bind('dragstart', function(event) { event.preventDefault(); });
       			if(state == 'on') {
       				img.animate({backgroundPosition: -53}, "slow", function() {
-      					img.attr('src', Drupal.settings.switch.switch_off_container_path);      					
+      					img.attr('src', Backdrop.settings.switch.switch_off_container_path);      					
       				});
       				state = 'off';
       				input.attr('checked', false);
@@ -44,7 +44,7 @@
       			else {
       				img.animate({backgroundPosition: 0}, "slow");      				
       				input.attr('checked', true);      				
-      				img.attr('src', Drupal.settings.switch.switch_on_container_path);
+      				img.attr('src', Backdrop.settings.switch.switch_on_container_path);
       				state = 'on';
       			}
       		});
